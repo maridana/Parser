d35b011d061ef20e708d4bd37579412c4bba7d15
Marina Danilova <marina.krasnopevtceva@gmail.com>
2024-04-09 21:20:06 +0300

    added linkparser session control

--- linkparser.py
+++ linkparser.py
@@ -9,11 +9,29 @@ import time
 
 from bs4 import BeautifulSoup as BS
 
-limit = 0
+try:
+    with open('links.json', 'r') as file_list:
+        link_list = json.load(file_list)
+except:
+    link_list = []
 
-number = 0
+try:
+    file = open('last_linksession.txt', 'r')
+    session = int(file.readline())
+    file.close()
+except:
+    session = 0
 
-link_list = []
+try:
+    file = open('last_limit.txt', 'r')
+    session_limit = int(file.readline())
+    file.close()
+except:
+    session_limit = 0
+
+number = session
+
+limit = session_limit
 
 while limit <= 50:
 
@@ -23,13 +41,30 @@ while limit <= 50:
     for link in html.find_all('h3', {'class': 'fl-l fs14 fw-b anime_ranking_h3'}):
         #print(number)
         link1 = link.select_one('a').get('href')
-        number += 1
         #print(link1)
         link_list.append({ number : link1 })
+
+        number += 1
+
+        file = open('last_linksession.txt', 'w')
+        file.write(str(number))
+        file.close()
+
+        file = open('last_linksession.txt', 'r')
+        session = int(file.readline())
+        file.close()
     
+    limit += 50
+
+    file = open('last_limit.txt', 'w')
+    file.write(str(limit))
+    file.close()
+
+    file = open('last_limit.txt', 'r')
+    session_limit = int(file.readline())
+    file.close()
 
     time.sleep(3)
-    limit += 50
 
     with open('links.json', 'w') as file_list:
         json.dump(link_list, file_list, indent=4)
